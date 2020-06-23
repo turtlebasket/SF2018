c2293ef098ec97d7d4cc5c2896a6ac4271c76795
turtlebasket <turtlebasket@gmail.com>
2020-06-23 12:43:37 -0700

    fix line endings (final)

--- Info_Gathering/analog_Sensor_Test/analog_Sensor_Test.ino
+++ Info_Gathering/analog_Sensor_Test/analog_Sensor_Test.ino
@@ -1,1 +1,15 @@
-/*Reads the input of analog input devices on the A0 pin*/const int inPin = A0;void setup() {  pinMode(inPin, INPUT);  Serial.begin(9600);}void loop() {  Serial.println(analogRead(inPin));  delay(200);}
+/*
+Reads the input of analog input devices on the A0 pin
+*/
+
+const int inPin = A0;
+
+void setup() {
+  pinMode(inPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(analogRead(inPin));
+  delay(200);
+}
--- Tests/N_Channel_MOSFET/N_Channel_MOSFET.ino
+++ Tests/N_Channel_MOSFET/N_Channel_MOSFET.ino
@@ -1,1 +1,15 @@
-/*Test of Switching current with N-channel mosfet*/const int switchMe = 12;void setup() {  pinMode(switchMe, OUTPUT);}void loop() {  digitalWrite(switchMe, HIGH);  delay(1000);  digitalWrite(switchMe, LOW);  delay(1000);}
+/*
+Test of Switching current with N-channel mosfet
+*/
+const int switchMe = 12;
+
+void setup() {
+  pinMode(switchMe, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(switchMe, HIGH);
+  delay(1000);
+  digitalWrite(switchMe, LOW);
+  delay(1000);
+}
